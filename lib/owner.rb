9545fd6c4e01a4ae97e25db2c05d98cc789ce288
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-26 00:07:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -27,8 +27,13 @@ class Owner
   def cats
     Cat.all.select {|cat| cat.owner == self}
   end  
+  
   def dogs
     Dog.all.select {|dog| dog.owner == self}
   end 
+  
+  def buy_cat
+  
+  end
   
 end
